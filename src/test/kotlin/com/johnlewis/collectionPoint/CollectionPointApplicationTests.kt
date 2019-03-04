418e16329ab22940ad6a6123ef00bd00808a5b48
Tom Scott <tomwscott@gmail.com>
2019-03-04 23:42:18 +0000

    Add the magic Spring Annotation Incantation so that the tests gets configured correctly? Looks like it's still dependent on a DB

--- src/test/kotlin/com/johnlewis/collectionPoint/CollectionPointApplicationTests.kt
+++ src/test/kotlin/com/johnlewis/collectionPoint/CollectionPointApplicationTests.kt
@@ -1,9 +1,15 @@
 package com.johnlewis.collectionPoint
 
+import com.johnlewis.collectionPoint.v1.CollectionPointApplication
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
 
-//@RunWith(SpringRunner::class)
-//@SpringBootTest
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@ContextConfiguration(classes = [CollectionPointApplication::class])
 class CollectionPointApplicationTests {
 
 	@Test
